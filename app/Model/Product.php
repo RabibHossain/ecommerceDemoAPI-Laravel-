16765cd047fee31a95bc746f7d470dd67d115658
RABIB HOSSAIN <galib.rabib@gmail.com>
2020-09-03 02:41:24 +0600

    product updated

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = [
+        'name',
+        'detail',
+        'stock',
+        'price',
+        'discount'
+    ];
     
     public function reviews()
     {
