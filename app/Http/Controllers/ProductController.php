4296d43a4dc0c81857198550efa692f2d48eae88
RABIB HOSSAIN <galib.rabib@gmail.com>
2020-09-03 02:14:39 +0600

    product created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
+// use Facade\FlareClient\Http\Response;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index', 'show');
+    }
     
     public function index()
     {
@@ -22,9 +30,21 @@ class ProductController extends Controller
     }
 
     
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        // return $request->all();
+        $product = new Product();
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+
+        return response([
+            'data' => new ProductResource($product)
+        ], Response::HTTP_CREATED);
+
     }
 
     
