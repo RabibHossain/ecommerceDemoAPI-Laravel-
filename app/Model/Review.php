700f867fee96a8d8ec2de98900b1113d50066f9d
RABIB HOSSAIN <galib.rabib@gmail.com>
2020-09-06 01:25:34 +0600

    reviewing a product

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    
+    protected $fillable = [
+        'customer',
+        'star',
+        'review'
+    ];
 
     public function product()
     {
